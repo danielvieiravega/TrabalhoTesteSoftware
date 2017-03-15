d167bb5cbd3646c071b0b2e04d6d1d884606f26c
Alelho <ac.alexandre.carvalho@outlook.com>
2017-03-15 18:54:07 -0300

    Adicionado arquivo Infrastructure para defininir os tipos de sensores. Alteração na classe Controle

---
+++ TrabalhoTesteSoftware/Infrastructure.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrabalhoTesteSoftware
+{
+    public enum Type_Sensor
+    {
+        Temperature = 0,
+        Pressure
+    };
+}
