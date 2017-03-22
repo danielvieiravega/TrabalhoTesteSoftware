a372bdfbad4ebb746476f125ee5169326a7ce9de
Daniel Vieira Vega <daniel.vega@acad.pucrs.br>
2017-03-22 18:51:57 -0300

    Adicionado projeto de testes

---
+++ TrabalhoTesteSoftware.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TrabalhoTesteSoftware.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TemperatureSensorIsTypeOfTemperature()
+        {
+            var controle = new Controle();
+
+            Assert.AreEqual(TypeSensor.Temperature, controle.TemperatureSensor.TypeSensor);
+
+        }
+    }
+}
